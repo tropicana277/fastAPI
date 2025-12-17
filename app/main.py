f1d48a1a005f4d65d09c215795df6b938290627c
tripicana277 <tripicana277@example.com>
2025-12-17 16:19:06 +0900

    FastAPI DynamoDB app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,4 @@
-import time
 from fastapi import FastAPI
-
-print("### main.py started ###")
 
 app = FastAPI()
 
@@ -9,8 +6,3 @@ app = FastAPI()
 @app.get("/health")
 def health():
     return {"status": "ok"}
-
-
-# プロセスを生かす
-while True:
-    time.sleep(10)
